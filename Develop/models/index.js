e297ad5c378394ebe579d2c7c0a0b93ab67f058c
Payton Whinnery <paytoncaliw@gmail.com>
2021-03-30 19:10:38 -0500

    adding index.js info for models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'productbelongtotag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tagbelongtoProduct'
+});
 
 module.exports = {
   Product,
